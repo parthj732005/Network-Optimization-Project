6767258838405a7dafc1b323f9a8e3a59408e66c
saratcbharadwaj <Sarat.Bharadwaj@iiitb.ac.in>
2025-11-22 17:43:08 +0530

    updated OptimizePage.jsx

--- frontend/src/pages/OptimizePage.jsx
+++ frontend/src/pages/OptimizePage.jsx
@@ -62,9 +62,7 @@ export default function OptimizePage() {
     if (!nCust) return setValidationError("Number of Customers is required.");
     if (nCust < 100 || nCust > 1000) return setValidationError("Number of Customers must be between 100 and 1000.");
     if (!nFc) return setValidationError("Number of FC Candidates is required.");
-    if (nFC < 3) return setValidationError("Number of FC Candidates must be atleast 3");
     if (!nk) return setValidationError("k (FCs to open) is required.");
-    if (nk < 3) return setValidationError("k (FCs to open) must be atleast 3");
     if (nK > nFc) return setValidationError(`k (${nK}) cannot be greater than FC Candidates (${nFc}).`);
 
     mutate({ num_customers: nCust, num_fc_candidates: nFc, k: nK });
@@ -180,6 +178,7 @@ export default function OptimizePage() {
     </div>
   );
 }
+
 
 
 
